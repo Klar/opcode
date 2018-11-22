7f13b64574c1b2902c3b8138080b4f0a0f3cea3c
afu <afuchs@backbone.ch>
2018-11-22 17:09:46 +0100

    restructure, get last blockheight and use that as a get txs

--- opCodes.py
+++ opCodes.py
@@ -6,80 +6,87 @@ import base64
 import json
 import requests
 
-
-epochtimeNow = time.time()
-epochtimeHourBack = epochtimeNow - 3600
-
-epochtimeNow = str(epochtimeNow)
-epochtimeHourBack = str(epochtimeHourBack)
-
-bitdbString = '{"v":3,"e":{"out.b1":"hex"},"db":["c"],"q":{"aggregate":[{"$match":{"$and":[{"blk.t":{"$gte":' + epochtimeHourBack + ',"$lte":'+ epochtimeNow + '}}]}}],"find":{},"project":{"tx.h":1,"out.str":1}}}'
-
-bitdbString64 = base64.b64encode(bitdbString.encode()).decode("utf-8")
-print(bitdbString64)
-
-url = "https://bitgraph.network/q/" + bitdbString64
 api_token = ''
-headers = {'key': api_token}
 
-response = requests.get(url, headers=headers)
-response = response.json()
-confirmedResponse = response['c']
+def bitd(querry):
 
-print(confirmedResponse)
+    bitdbString64 = base64.b64encode(querry.encode()).decode("utf-8")
+    # print(bitdbString64)
 
-opoutput = dict()
-opreturn = dict()
+    url = "https://bitgraph.network/q/" + bitdbString64
+    headers = {'key': api_token}
 
-opoutput["OP_DUP"] = 0
-opoutput["OP_HASH160"] = 0
+    response = requests.get(url, headers=headers)
+    response = response.json()
+    confirmedResponse = response['c']
 
-for tx in confirmedResponse:
-    print(tx)
-    txID = tx["tx"]["h"]
-    for data in tx["out"]:
-        data = data["str"].split(" ")
-        if data[0] == "OP_DUP":
-            opoutput["OP_DUP"] = opoutput["OP_DUP"] + 1
-        elif data[0] == "OP_RETURN":
-            # opreturndict = dict()
-            print(data)
-            # print(len(data))
-            if len(data) == 2:
-                ascii = data[1].decode("hex").encode("utf-8")
-                print(ascii)
-            elif len(data) == 3:
-                prefix = data[1]
-                print(prefix)
-                if len(data[2]) == 64:
-                    txID = data[2]
-                    print(txID)
-                else:
-                    ascii = data[2].decode("hex").encode("utf-8")
-                    print(ascii)
-            elif len(data) == 4:
-                prefix = data[1]
-                txID = data[2]
-                if len(data[3]) > 1:
-                    ascii = data[3].decode("hex").encode("utf-8")
-                    print(ascii)
-                print(prefix)
-                print(txID)
-            print("\n\n")
-            # opreturndict["txID"] = txID
-            # opreturn["prefix"] =
-            # opreturn["txID"] = txID
-            # opreturn[txID] = opreturndict
-        elif data[0] == "OP_HASH160":
-            opoutput["OP_HASH160"] = opoutput["OP_HASH160"] + 1
-        else:
-            opoutput[data[0]] = 0
+    return(confirmedResponse)
 
-prefixes = dict()
+def getOutput(getOpTxs):
+    opoutput = dict()
+    opreturn = dict()
 
-# print(opreturn)
+    opoutput["OP_DUP"] = 0
+    opoutput["OP_HASH160"] = 0
+    opoutput["OP_RETURN"] = 0
 
-# print(len(prefixes))
-opoutput["OP_RETURN"] = prefixes
+    for tx in getOpTxs:
+        txID = tx["tx"]["h"]
+        print(txID)
+        for data in tx["out"]:
+            data = data["str"].split(" ")
+            if data[0] == "OP_DUP":
+                opoutput["OP_DUP"] = opoutput["OP_DUP"] + 1
+            elif data[0] == "OP_HASH160":
+                opoutput["OP_HASH160"] = opoutput["OP_HASH160"] + 1
+            elif data[0] == "OP_RETURN":
+                opoutput["OP_RETURN"] = opoutput["OP_RETURN"] + 1
+                # opreturndict = dict()
+                print(data)
+                # print(len(data))
+                # if len(data) == 2:
+                #     ascii = data[1].decode("hex").encode("utf-8")
+                #     # print(ascii)
+                # elif len(data) == 3:
+                #     prefix = data[1]
+                #     # print(prefix)
+                #     if len(data[2]) == 64:
+                #         txID = data[2]
+                #         # print(txID)
+                #     else:
+                #         ascii = data[2].decode("hex").encode("utf-8")
+                #         # print(ascii)
+                # elif len(data) == 4:
+                #     prefix = data[1]
+                #     txID = data[2]
+                #     if len(data[3]) > 1:
+                #         ascii = data[3].decode("hex").encode("utf-8")
+                #         print(ascii)
+                #     # print(prefix)
+                #     # print(txID)
+                # print("\n\n")
+                # opreturndict["txID"] = txID
+                # opreturn["prefix"] =
+                # opreturn["txID"] = txID
+                # opreturn[txID] = opreturndict
+            else:
+                opoutput[data[0]] = 0
 
-print(json.dumps(opoutput, indent=4, sort_keys=True))
+    # prefixes = dict()
+
+    # print(opreturn)
+
+    # print(len(prefixes))
+    # opoutput["OP_RETURN"] = prefixes
+
+    print(json.dumps(opoutput, indent=4, sort_keys=True))
+
+getLatestBlockQuerry = '{"v":3,"q":{"db":["c"],"find":{},"limit":1},"r":{"f":"[.[] | .blk | { current_blockheight: .i} ]"}}'
+
+getLatestBlockQuerry = bitd(getLatestBlockQuerry)
+
+current_blockheight = getLatestBlockQuerry[0]["current_blockheight"]
+print(current_blockheight)
+getOpTxs = '{"v":3,"e":{"out.b1":"hex"},"q":{"db":["c"],"find":{"blk.i":{"$eq":'+ str(current_blockheight) +'}},"limit":10,"project":{"tx.h":1,"out.str":1}}}'
+getOpTxs = bitd(getOpTxs)
+getOutput(getOpTxs)
